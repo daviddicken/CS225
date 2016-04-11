b8ccb2bec5825170639968c6ef43e35774bd5077
David Dicken <craig5pam@yahoo.com>
2016-04-11 15:05:57 -0700

    added lab1 shell script

---
+++ lab1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Please enter a number: "
+read NUM
+
+expr $NUM + 0 2> /dev/null
+if [ $? = 0 ] ;then 
+     if   [[ "$NUM" -gt '5' ]] ;then
+              echo "${NUM} is greater then 5"
+     elif [[ "$NUM" -lt '5' ]] ;then
+              echo "${NUM} is less then 5"
+     elif [[ "$NUM" -eq '5' ]] ;then
+              echo "${NUM} is equal to 5"
+     else 
+              echo "Not a valid number."
+      fi
+else
+      echo "Not a valid number."
+fi
+
+
+

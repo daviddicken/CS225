d6114aac2e64b7beb2de85d90e226ce19a1a94e1
David Dicken <craig5pam@yahoo.com>
2016-04-12 02:04:46 -0700

    added line to test if input is empty for lab2.sh

--- lab2.sh
+++ lab2.sh
@@ -3,17 +3,21 @@
 echo "Please enter a number: "
 read NUM
 
-expr $NUM + 1 1>/dev/null                   #test if integer
-if [ $? = 0 ] ;then
-   if [[ "$NUM" != '0' ]] ;then
-      if ((NUM%2==0)) ;then
-         echo "${NUM} is a even number."
-      else
-         echo "${NUM} is a odd number."
-      fi
-   else
-     echo "${NUM} is a even number."
-   fi 
+if [[ -z $NUM ]] ;then
+   echo "No input."
 else
-  echo "Invalid number."
+  expr $NUM + 1 1>/dev/null                   #test if integer
+   if [ $? = 0 ] ;then
+      if [[ "$NUM" != '0' ]] ;then
+         if ((NUM%2==0)) ;then
+            echo "${NUM} is a even number."
+         else
+            echo "${NUM} is a odd number."
+         fi
+      else
+        echo "${NUM} is a even number."
+      fi 
+   else
+     echo "Invalid number."
+   fi
 fi

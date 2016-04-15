e45e714fc4b4102688197bca659d443e0c1e3428
David Dicken <craig5pam@yahoo.com>
2016-04-15 07:24:52 -0700

    adding lab3.sh

---
+++ lab3.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ "$USER" =  "root" ]] ;then
+	echo "It is insecure to run this program as root"
+	exit
+elif [[ "$UID" -lt "500" ]] ;then
+	echo "The user id is less then 500"
+	exit
+fi
+
+home=$(grep "$USER" /etc/passwd | cut -f6 -d:)
+if [ $? = 0 ] ;then
+	size=$(du -sh "$home" | awk -F' ' '{ print $1 }')
+	echo "$home is $size"  
+else
+	echo "Home directory doesn't exist or is not readable."
+fi
+
+ 

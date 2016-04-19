8b29d1a687bae3490d15d46296249e3809deac5d
David Dicken <craig5pam@yahoo.com>
2016-04-19 10:16:31 -0700

    finished lab4.sh

---
+++ lab4.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Please enter a number between 50 - 100"
+read NUM
+
+CLEAN=$(echo $NUM | sed 's/[.].*$//')
+
+if [[ $CLEAN =~ ^[0-9]+$ && $CLEAN -gt '49' && $CLEAN -lt '101' ]] ;then
+	echo "${NUM} is a number between 50 - 100."
+else
+	echo "Sorry ${NUM} is not a number between 50 - 100"
+fi
+
+

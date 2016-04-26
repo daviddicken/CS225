152c3b78899e371723e3351012e629066b8209ea
David Dicken <craig5pam@yahoo.com>
2016-04-26 15:55:21 -0700

    changed lab6 if statement to ! grep statement

--- lab6.sh
+++ lab6.sh
@@ -1,41 +1,33 @@
 #!/bin/bash
 
-xmlLIST=$( grep "filename" ~/medialab/media.xml | awk -F'>' '{ print $2 }' | awk -F'<' '{ print $1 }' | sort | uniq | grep -vwE "(NULL)" )
+xmlLIST=$( grep "filename" ~/medialab/media.xml | awk -F'[<|>]' '{ print $3 }' | sort | uniq | grep -vwE "(NULL)" )
 
-labLIST=$( ls ~/medialab | grep "\.mp"  )
+labLIST=$( ls -1 ~/medialab )
 
 LAB=~/medialab 
 XML=~/medialab/media.xml
+IFS=$'\n'
 
-rm -f NOTnMEDIALAB.txt
-rm -f NOTnXML.txt
+echo "Files not in media.xml:"
+for LINE in $labLIST ;do
+	if ! grep -q "$LINE" ~/medialab/media.xml
+        then
+                echo $LINE
+                (( NOTnXMLcounter++ ))
+        fi
+done
 
-echo "Files not in medialab directory:" >> NOTnMEDIALAB.txt
-echo "Files not in media.xml:" >> NOTnXML.txt 
-
+echo "Files not in medialab directory:"
 for LINE in $xmlLIST ;do
 	if [[ -e $LAB/$LINE ]]
 	then
 	:	
 	else	
-		echo $LINE >> NOTnMEDIALAB.txt
+		echo $LINE
 		(( NOTnLABcounter++ ))
 	fi
 done
 
-for LINE in $labLIST ;do
-	if [[ -e $XML/$LINE ]]
-	then
-	:
-	else
-		echo $LINE >> NOTnXML.txt
-		(( NOTnXMLcounter++ ))
-	fi
-done
-
-
-cat $( pwd )/NOTnXML.txt
-cat $( pwd )/NOTnMEDIALAB.txt
 echo "$NOTnXMLcounter media files in medialab directory that are NOT listed in media.xml."
 echo "$NOTnLABcounter media files in media.xml that are NOT in medialab directory."
 
